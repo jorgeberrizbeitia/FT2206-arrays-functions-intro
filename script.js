6862ec31cd0b0e1f6a0e2e9a10fa0b76b93b0cbb
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-06-29 13:00:24 +0200

    functions-basic

--- script.js
+++ script.js
@@ -72,3 +72,173 @@ console.log( countries.includes("Dinamarca") ) // false
 // estos dos metodos tambien aplican para strings
 
 // console.log( countries.toUpperCase() ) // :(
+
+
+// MUTABILIDAD
+
+let str = "hola" // stings y todos los elementos primitivos son inmutables
+
+console.log(str[0])
+
+str[0] = "H"
+console.log(str)
+
+str = str[0].toUpperCase() + str.slice(1)
+console.log(str)
+
+
+let friends = ["Ross", "Monica", "Phoebe", "Chandler", "Joey", "Rachel"]
+
+friends[0] = "Mike"
+
+console.log(friends)
+// arrays y objectos en JS son mutables
+
+console.log( friends.slice(2, 4) )
+console.log( friends )
+
+
+// pop . elimina del final
+// y shift . elimina del inicio
+
+let elementRemoved = friends.pop() // eliminando el ultimo elemento del array
+console.log("luego del pop", friends)
+console.log("elemento removido", elementRemoved)
+
+let firstElemRemoved = friends.shift()
+console.log("luego del shift", friends)
+console.log("elemento removido", firstElemRemoved)
+
+// friends.pop()
+// friends.pop()
+// friends.pop()
+// friends.pop()
+// console.log(friends) // ha eliminado todos!
+
+// console.log( friends.pop() )
+// console.log( friends )
+
+
+// push . agrega al final
+// y unshift . agrega al inicio
+
+friends.push("Janice")
+console.log(friends)
+
+friends.unshift("Gunther")
+console.log(friends)
+
+
+// .splice
+// tanto eliminar como agregar elementos internos del array
+
+friends.splice(2, 2) // ejemplo solo de borrar
+console.log(friends)
+
+friends.splice(1, 0, "Emily") // ejemplo solo de agregar
+console.log(friends)
+
+friends.splice(3, 1, "Robert", "Nona")
+console.log(friends)
+
+
+// REFERENCIAS
+
+let age1 = 40;
+let age2 = 40;
+
+console.log( age1 === age2 )
+
+
+let arr1 = [10, 20, 30] // ref 1234
+let arr2 = [10, 20, 30] // ref abcd
+
+console.log( arr1 === arr2 )
+// cuando comparamos objetos en JS, compara por su referencia
+
+
+let arr3 = arr1; // .??? aqui no estamos copiando el array/clonando
+// estamos asignando la misma referencia del array indicado
+
+// hacer lo de arriba es mala practica, pero JS muchas veces lo hacer tras camaras y hay que tenerlo en cuenta
+console.log(arr3) // ref 1234
+
+arr3.pop()
+arr3.pop()
+arr3.pop()
+
+console.log(arr3);
+
+console.log(arr1)
+
+console.log( arr3 === arr1) // true, ambos tienen la misma referencia
+
+
+
+
+let numArr1 = [10, 20, 30] // ref 1234
+let numArr2 = [10, 20, 30] // ref abcd
+
+console.log( numArr1 === numArr2 )
+
+
+let check = true;
+
+for (let i = 0; i < numArr1.length; i++) {
+    if (numArr1[i] !== numArr2[i]) {
+        check = false
+    }
+}
+
+console.log(check)
+
+// stringify
+console.log( JSON.stringify(numArr1) === JSON.stringify(numArr2) )
+
+
+console.log("FUNCIONES")
+
+// FUNCIONES
+
+// declaramos una funcion (creamos)
+function functionName( /* parametros opcionales*/ ) {
+    // lo que se ejecuta cada vez que invocamos la funcion
+    // opcionalmente tendremos un return que saca un valor de la funcion
+}
+
+// invocar la funcion
+functionName(/* argumentos opcionales*/)
+functionName(/* argumentos opcionales*/)
+functionName(/* argumentos opcionales*/)
+
+
+
+function printName(name) {
+    // let name = ???
+    // console.log(name)
+    let sentence = `Hola ${name}`
+    // console.log(`Hola ${name}`)
+    return sentence // no saca el nombre de la variable
+}
+
+let sentence1 = printName("Ivan")
+let sentence2 = printName("Caro")
+let sentence3 = printName("Rebeca")
+let sentence4 = printName("Miguel")
+let sentence5 = printName("Malva")
+
+console.log([sentence1, sentence2, sentence3, sentence4, sentence5])
+
+
+
+function sumNumbers(num1, num2) {
+    // console.log(num1, num2)
+    let sum = num1 + num2
+    return sum;
+}
+
+
+console.log( 9 ); // 9
+
+let sumOutside = sumNumbers(8, 10); // 18
+console.log(sumOutside * 1000)
